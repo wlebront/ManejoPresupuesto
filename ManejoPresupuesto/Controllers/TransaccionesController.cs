0305ce7af7350a1d9dda62763f2f7be40eeca5c2
William Lebron <WilliamLebron23@hotmail.com>
2022-05-01 15:30:03 -0400

    Creando un Submenu

--- ManejoPresupuesto/Controllers/TransaccionesController.cs
+++ ManejoPresupuesto/Controllers/TransaccionesController.cs
@@ -37,6 +37,29 @@ namespace ManejoPresupuesto.Controllers
         }
 
 
+        public IActionResult Semanal() 
+        {
+            return View();
+        }
+
+        public IActionResult Mensual()
+        {
+            return View();
+        }
+
+        public IActionResult ExcelReporte()
+        {
+            return View();
+        }
+
+        public IActionResult Calendario()
+        {
+            return View();
+        }
+
+      
+
+
         public async Task<IActionResult> Crear() 
         {
             var usuarioId = servicioUsuarios.ObtenerUsuarioId();
