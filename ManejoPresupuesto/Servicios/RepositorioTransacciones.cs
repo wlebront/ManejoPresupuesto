ded5b62cb46a1ee2f9791593192beba12a5623be
William Lebron <WilliamLebron23@hotmail.com>
2022-05-05 17:11:34 -0400

    Reporte Mnesual - Query

--- ManejoPresupuesto/Servicios/RepositorioTransacciones.cs
+++ ManejoPresupuesto/Servicios/RepositorioTransacciones.cs
@@ -13,6 +13,7 @@ namespace ManejoPresupuesto.Servicios
         Task Crear(Transaccion transaccion);
         Task<IEnumerable<Transaccion>> ObtenerPorCuentaId(ObtenerTransaccionesPorCuenta modelo);
         Task<Transaccion> ObtenerPorId(int id, int usuarioId);
+        Task<IEnumerable<ResultadoObtenerPorMes>> ObtenerPorMes(int usuarioId, int año);
         Task<IEnumerable<ResultadoObtenerPorSemana>> ObtenerPorSemana(ParametroObtenerTransaccionesPorUsuario modelo);
         Task<IEnumerable<Transaccion>> ObtenerPorUsuarioId(ParametroObtenerTransaccionesPorUsuario modelo);
     }
@@ -130,6 +131,19 @@ namespace ManejoPresupuesto.Servicios
                     ", modelo);
         }
 
+        public async Task<IEnumerable<ResultadoObtenerPorMes>> ObtenerPorMes(int usuarioId,
+            int año)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryAsync<ResultadoObtenerPorMes>(@"
+            SELECT MONTH(FechaTransaccion) as Mes,
+            SUM(Monto) as Monto, cat.TipoOperacionId
+            FROM Transacciones
+            INNER JOIN Categorias cat
+            ON cat.Id = Transacciones.CategoriaId
+            WHERE Transacciones.UsuarioId = @usuarioId AND YEAR(FechaTransaccion) = @Año
+            GROUP BY Month(FechaTransaccion), cat.TipoOperacionId", new { usuarioId, año });
+        }
 
 
         public async Task Borrar(int id)
