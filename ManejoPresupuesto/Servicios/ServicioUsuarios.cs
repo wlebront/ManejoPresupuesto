a54a4ebc216c7e346b3a654302049ab9728f4431
William Lebron <WilliamLebron23@hotmail.com>
2022-05-13 17:11:05 -0400

    Login - Implementado

--- ManejoPresupuesto/Servicios/ServicioUsuarios.cs
+++ ManejoPresupuesto/Servicios/ServicioUsuarios.cs
@@ -1,16 +1,35 @@
-﻿namespace ManejoPresupuesto.Servicios
+﻿using System.Security.Claims;
+
+namespace ManejoPresupuesto.Servicios
 {
+
     public interface IServicioUsuarios
     {
         int ObtenerUsuarioId();
     }
-    public class ServicioUsuarios: IServicioUsuarios 
-    {
 
-        public int ObtenerUsuarioId() 
+    public class ServicioUsuarios : IServicioUsuarios
+    {
+        private readonly HttpContext httpContext;
+
+        public ServicioUsuarios(IHttpContextAccessor httpContextAccessor)
         {
-            return 1;
+            httpContext = httpContextAccessor.HttpContext;
+        }
+
+        public int ObtenerUsuarioId()
+        {
+            if (httpContext.User.Identity.IsAuthenticated)
+            {
+                var idClaim = httpContext.User
+                        .Claims.Where(x => x.Type == ClaimTypes.NameIdentifier).FirstOrDefault();
+                var id = int.Parse(idClaim.Value);
+                return id;
+            }
+            else
+            {
+                throw new ApplicationException("El usuario no está autenticado");
+            }
         }
     }
-    
 }
